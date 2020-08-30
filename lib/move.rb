083f9e4448763231b9ee1dc97fc4b248bc86e32b
Mike Barberry <mikebarberry@protonmail.com>
2020-08-30 01:50:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,4 +12,9 @@ def input_to_index(input)
   input.to_i - 1
 end
 
+# write input into array
+def move(array, index, value="X")
+  array.push[index] = value
+end
+
 # code your input_to_index and move method here!
